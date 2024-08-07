db3f10469f11112597e6cc74966af72288eb9900
Tbharvesh <tanisha.bharvesh@gmail.com>
2024-08-08 01:03:09 +0530

    having clause

--- Aggregate_Group_Having_Clause.sql
+++ Aggregate_Group_Having_Clause.sql
@@ -52,4 +52,58 @@ group by dept
 order by tot_emp desc
 
 
--- 
+-- Having clause  
+-- Find tot sal and no. of emp , having sal >5000 and no.of emp not eql 1
+select dept,sum(sal),count(*)
+from emp
+group by dept 
+having sum(sal) > 5000 and count(*) != 1
+
+
+select dept, sum(Sal) as tot_sal
+from emp
+where dept is not null
+GROUP BY dept 
+having tot_sal between 2000 and 6000
+
+-- Write a SQL query to count the number of duplicate first name as duplicate_count in the employee table.
+select fname , count(*) as duplicate_cnt
+from emp
+group by fname
+having count(*)>1
+
+-- Find the departments with more than 5 employees as total_employees from the employee table
+select dept,count(*)
+from emp
+group by dept
+having count(*) > 5
+
+-- Determine the department with at least 3 employees earning a salary 30000 or more total_employees from as the employee table.
+select dept , count(*) as tot_emp
+from emp
+where  sal>=30000 
+group by dept
+having count(*) > 3;
+
+select dept,max(Sal)
+from emp
+group by dept 
+having max(sal)> 6000;
+
+-- Find the department where the highest salary (as highest_salary) isat least twice the lowest salary
+select dept , max(sal) , min(sal)
+from emp
+group by dept
+having max(sal) >= 2*min(sal);
+
+-- Determine the department with at least 5 employees (astotal_employees) where the total salary expense (astotal_salary_expense ) is greater than 100000 in the employee table
+select dept , count(*) ,sum(sal)
+from emp 
+group by dept
+HAVING count(*) >=5 and sum(sal) >10000
+
+-- Determine the departments with more than 4 employees (astotal_employees) where the difference between the maximum andminimum salary is greater than 25000 (salary_difference) from theemployee table.
+select dept,count(*) , max(sal), min(sal)
+from emp 
+group by dept 
+having count(*)>4 and  (max(sal)-min(sal)) > 2500
